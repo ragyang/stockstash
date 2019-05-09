87f8f2ed2d61df0a4485bc6c7c81a6b65801ee7b
chrisragasa <ragasa.chris@gmail.com>
2019-05-08 20:26:38 -0700

    added gitignore, created templates for login/registration, added static folder

--- stockstash/__init__.py
+++ stockstash/__init__.py
@@ -3,8 +3,8 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-app.config['MONGO_DBNAME'] = 'REDACTED'
-app.config['MONGO_URI'] = 'REDACTED'
+app.config['MONGO_DBNAME'] = 'stockstash'
+app.config['MONGO_URI'] = 'mongodb://admin:ZQTymqcbB6aZgtRY@ds149056.mlab.com:49056/stockstash'
 
 #init the database
 mongo = PyMongo(app)
